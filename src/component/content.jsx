8fa2750bc5f1d1717b55e7e1eca714ee285b5ebd
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-04-08 18:40:21 +0330

    css grid

--- src/component/content.jsx
+++ src/component/content.jsx
@@ -3,7 +3,13 @@ import '../css/content.css';
 const Content = () => {
     return ( 
         <div className='content'>
-            c
+            <div className='block_S_top'>
+                <div className='fill_outer'></div>
+            </div>
+            <div className='block_S_bottom'>S</div>
+            <div className='block_P'>P</div>
+            <div className='block_D'>D</div>
+            <div className='block_F'>F</div>
         </div>
      );
 }
