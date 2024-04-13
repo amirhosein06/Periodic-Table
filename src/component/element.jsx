1f1d2289b9002d7f224bef98b4cd08d799f8b215
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-04-13 20:10:32 +0330

    change data

--- src/component/element.jsx
+++ src/component/element.jsx
@@ -1,7 +1,15 @@
 import '../css/element.css';
 
 const Element = ({element}) => {
-    return ( <h1>{element.symbol}</h1> );
+    return (
+    <>
+    <h1>{element.symbol}</h1>
+    </>
+    );
 }
  
 export default Element;
+
+
+//    <img src={`https://www.astronomy.ohio-state.edu/pogge.1/TeachRes/HandSpec/${element.name.toLowerCase()}.png`} alt="Chemical Elements" /> 
+//    <img src={`https://images-of-elements.com/${element.name.toLowerCase()}.jpg`} alt="images of Elements" /> 
